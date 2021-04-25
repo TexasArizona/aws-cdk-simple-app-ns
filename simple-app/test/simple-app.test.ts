46d8752e3c7c8869944536a89b7e00af7eb0799f
Ubuntu <ubuntu@ip-172-31-37-52.eu-central-1.compute.internal>
2021-04-25 16:27:55 +0000

    Full simple-app.test.ts

--- simple-app/test/simple-app.test.ts
+++ simple-app/test/simple-app.test.ts
@@ -44,3 +44,11 @@ test('Simple App Stack', () => {
     )
   );
 });
+test('Stack create a S3 bucket', () => {
+  // ARRANGE
+  const app = new cdk.App();
+  // ACT
+  const stack = new SimpleApp.SimpleAppStack(app, 'MyTestStack');
+  // ASSERT
+  expect(stack).toHaveResource('AWS::S3::Bucket');
+});
