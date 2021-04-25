048eaceaa3368c2e5447c214b90d716c39b69c15
Ubuntu <ubuntu@ip-172-31-37-52.eu-central-1.compute.internal>
2021-04-25 10:03:00 +0000

    add bucket

--- simple-app/lib/simple-app-stack.ts
+++ simple-app/lib/simple-app-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+const bucket = new Bucket(this, 'MySimpleAppBucket', {
+      encryption: BucketEncryption.S3_MANAGED,
+    });
+    
   }
 }
