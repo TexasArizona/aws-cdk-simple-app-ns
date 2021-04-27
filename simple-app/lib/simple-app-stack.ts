6b494ebd51223c00a2aa7a9f9671fdf6e1322ed7
Ubuntu <ubuntu@ip-172-31-39-167.eu-central-1.compute.internal>
2021-04-27 18:50:38 +0000

    17. Creating and deploying our TypeScript Lambda Content to the CDK Lambda

--- simple-app/lib/simple-app-stack.ts
+++ simple-app/lib/simple-app-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
+import * as lambda from '@aws-cdk/aws-lambda-nodejs';
+import * as path from 'path';
+import { Runtime } from '@aws-cdk/aws-lambda';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,6 +10,15 @@ export class SimpleAppStack extends cdk.Stack {
 
 const bucket = new Bucket(this, 'MySimpleAppBucket', {
       encryption: BucketEncryption.S3_MANAGED,
+    });
+    
+    const getPhotos = new lambda.NodejsFunction(this, 'MySimpleAppLambda', {
+      runtime: Runtime.NODEJS_12_X,
+      entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
+      handler: 'getPhotos',
+      environment: {
+        PHOTO_BUCKET_NAME: bucket.bucketName,
+      },
     });
     
     new cdk.CfnOutput(this, 'MySimpleAppBucketNameExport', {
